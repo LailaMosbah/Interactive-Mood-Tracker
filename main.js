c8a1e99f11e6ea83a6af2e087ef8bcb852006d92
LailaMosbah <lylymsbah2@gmail.com>
2025-05-06 16:00:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,80 @@
 let btns = document.querySelectorAll(".color")
 let show = document.querySelector(".show")
-//localStorage.clear()
+let copy = document.querySelector("#copy")
+let voice = document.querySelector("#voice")
 
-if(localStorage.getItem("color"))
-{
-    show.style.backgroundColor = localStorage.getItem("color")
-    document.querySelector(`[data-color = ${localStorage.getItem("color")}]`).classList.add('active')
+
+
+
+
+show.textContent = "A Message For You"
+show.style.color = 'black'
+show.style.backgroundColor ='#f4f4f4'
+
+
+
+/// display quote when btn is selected
+
+btns.forEach(btn =>{
+    btn.addEventListener('click',async function(e){
+        //remove active class from all btns
+        show.style.color = 'white'
+
+        btns.forEach(btn => {
+            btn.classList.remove('active')
+        });
+
+        e.target.classList.add('active')
+
+        show.style.backgroundColor = e.target.dataset.color;
+        //copy.style.hover.backgroundColor = e.target.datas
+
+        let quotes = await getQuotes(`${e.target.dataset.emotion}.txt`)
+        let randomQuote = quotes[Math.floor(Math.random() * quotes.length )]
+        show.textContent = randomQuote;
+
+    })
+})
+
+
+
+// Function to fetch quotes from a text file
+
+async function getQuotes(file){
+    try{
+        let response = await fetch(file)
+        let data = await response.text()
+        console.log(data)
+        data = data.split("\n").filter(quote => quote.trim() !== "");
+        return data
+    }
+    catch(error){
+        console.error("Error fetching quotes:", error);
+        return ["No quotes available"];
+    }
 }
 
-btns.forEach(btn => {
-    btn.addEventListener("click",function(e){
-        btns.forEach(element => {
-            element.classList.remove("active")
-        });
-        e.target.classList.add("active")
-        localStorage.setItem("color",e.target.dataset.color)
-        console.log(localStorage.getItem("color"))
-        show.style.backgroundColor = e.target.dataset.color
-    })
-}); 
+// Function to copy Quote
+function copyQuote() {
+    const quote = document.querySelector('.show').textContent;
+    navigator.clipboard.writeText(quote).then(()=>{
+        alert('the Quote is coppied ssuccessfully')
+    }
+
+    )
+}
+
+
+// Function to speak the quote
+function speakQuote() {
+    const quote = document.querySelector('.show').textContent;
+    const voice = new SpeechSynthesisUtterance(quote);
+    console.log(voice)
+    window.speechSynthesis.speak(voice);
+}
+
+
+copy.addEventListener('click', copyQuote);
+voice.addEventListener('click', speakQuote);
+
+
